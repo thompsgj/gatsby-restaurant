81b1254622d4fc8c3ba6a488976648bc47b45ce4
Greg <thompsgj@yahoo.com>
2019-10-26 13:38:21 +0900

    Added Navbar + Error Fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,41 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+//import "./layout.css"
+import { createGlobalStyle } from "styled-components"
+import Navbar from "../components/globals/navbar"
+//import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <React.Fragment>
+      <GlobalStyle />
+      <Navbar />
+      {children}
+    </React.Fragment>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family:'Open Sans', sans-serif;
+  color: #262626;
+  background: #fff;
+}
+:root {
+  --mainOrange: #f15025;
+}
+body {
+  font-family: "Caveat", cursive;
+  color: black;
+  background: white;
+}
+
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { FaBeer } from "react-icons/fa"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "styled-components"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h3>Hello from Gatsby</h3>
+    <p>
+      Let's go for a <FaBeer />
+    </p>
+    <ButtonWrapper>Click Me</ButtonWrapper>
   </Layout>
 )
+
+const ButtonWrapper = styled.button`
+  background: blue;
+  color: white;
+`
 
 export default IndexPage
